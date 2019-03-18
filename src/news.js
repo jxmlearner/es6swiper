b4ac5b606327b5e5efaa43c5d173b60c90297b10
jxmlearner <410958040@qq.com>
2019-03-18 17:49:28 +0800

    news

--- src/news.js
+++ src/news.js
@@ -3,7 +3,7 @@ import Swiper from 'swiper' // swiper模块
 import 'swiper/dist/css/swiper.css' // swiper的样式
 import './assets/css/news.styl'
 
-new Swiper('header', {
+new Swiper('.news-header', {
     freeMode: true,
     slidesPerView: 'auto',
     freeModeSticky: true
